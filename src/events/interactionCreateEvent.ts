ba5356d395e997c4395e8bd636a309fa4243e011
Chris <jochchristopher@gmail.com>
2024-01-03 23:35:43 +0100

    catch error to avoid crash

--- src/events/interactionCreateEvent.ts
+++ src/events/interactionCreateEvent.ts
@@ -54,10 +54,14 @@ export class InteractionCreateHandler {
             await this.handleRawInteraction(commandData, interaction, resolver);
         } catch (error: any) {
             console.error(error);
-            if (interaction.replied || interaction.deferred) {
-                await interaction.followUp({ content: "There was an error while executing this command!", ephemeral: true });
-            } else {
-                await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
+            try {
+                if (interaction.replied || interaction.deferred) {
+                    await interaction.followUp({ content: "There was an error while executing this command!", ephemeral: true });
+                } else {
+                    await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
+                }
+            } catch (err: any) {
+                console.error(error);
             }
         }
     }
